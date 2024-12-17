084bfd642b713ad25b045cdb77bd13966b3b417e
rfzn <rafizansamian@gmail.com>
2024-12-17 10:05:19 +0800

    Update Guanotestv1.py

--- Guanotestv1.py
+++ Guanotestv1.py
@@ -10,6 +10,32 @@ def load_data():
     data = pd.read_csv('SYP.csv')
     return data
 
+def get_soil_class_table():
+    data = {
+        "Kelas Tanah": ["Kelas 1 (Sangat sesuai)"] * 2 + ["Kelas 2 (Sesuai)"] * 28 + ["Kelas 3 (Kurang sesuai)"] * 30,
+        "Jenis Tanah": ["SELANGOR", "KANGKONG", "BRIAH", "TELONG", "SEGAMAT", "KUANTAN", "BENTA", "SK. MAS", 
+                        "KATONG", "SABAK", "YONG PENG", "TOM YONG", "KEMUNING", "JERANGAU", "JEMBANG", "JELAI", 
+                        "CHAMP", "MUSANG", "SEMPAKA", "KAMPONG KOLAM", "MASSA", "LENGKAWI", "NENASI", "BATANG MERBAU", 
+                        "LIMBAT", "TEBOK", "KALLA", "COLLUVIUM", "BUKIT", "ORGANIC ALLUVIUM", "LOCAL ALLUVIUM", 
+                        "BUNGOR", "CHEROK", "SEMUPURNA/IMD", "BATANG", "BESAI", "RAU", "TAPAH", "RASAU", "BUNGOR", 
+                        "ROMPIN", "RUDUA", "MUNCHONG/L", "BUNGOR/S", "DURIAN", "CHENIAN/S", "DUTALAN", "BATU LAPAN", 
+                        "HARRADIL", "MERAPOH/L", "MT. HAIL", "MALACCA", "HARRADS", "GAJAH MATI", "MERAPOH", "HARAD/ACAD", 
+                        "KEDAH", "TAY", "SEREMBAN", "KANTIS", "KALI BUKIT", "DURIAN ALAM", "SEMPORNA/S", "BATU/M", 
+                        "PAGOH", "ORGANIC CLAY MUCK", "SANDY COLLUVIUM", "SRANTI", "KUALA BERANG", "BUKIT TUKU", 
+                        "KAMP. KUBUR", "ULU TIRAM", "JABIL", "SEDIRANG", "MARANG", "TAPAH", "BINA", "KUALA BRANG", 
+                        "PEAT/D"],
+        "Kod": ["SLR", "KGR", "BRH", "TLG", "SGT", "KTN", "BNT", "SMS", "KTG", "SBK", "YPG", "TYN", "KMG", "JRN", "JMB", 
+                "JLC", "CPG", "MUS", "SPA", "KKL", "MSI", "LKI", "NBI", "MRB", "LBT", "TBK", "KLL", "COL", "BKT", "ORA", 
+                "LRA", "BGR", "CRK", "SPAMD", "BTG", "BSH", "RAU", "TGH", "RAS", "RGR", "RPN", "RDA", "MUNL", "BRS", 
+                "DUR", "CHNS", "DTL", "BNL", "HRDL", "MRPL", "MTH", "MLC", "HRDS", "GMH", "MRP", "HRA", "KDH", "TAY", 
+                "SRB", "KNT", "KBUK", "DAL", "SMP", "BTM", "PGH", "OCM", "SCL", "SRA", "KBG", "BKTK", "KPR", "ULT", 
+                "JBL", "SDR", "MRG", "TGH", "BNA", "KLG/KBG", "PET/D"],
+        "Kumpulan": [1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+                     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 
+                     5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+    }
+    return pd.DataFrame(data)
+
 def predict_syp(df, rainfall_zone, soil_class, topography, year):
     """
     Predict Site Yield Potential based on input parameters
@@ -74,28 +100,31 @@ def main():
     # Load data
     df = load_data()
     
-    # Sidebar for input
-    st.sidebar.header('Input Parameters')
+    # Create columns for input
+    col1, col2, col3 = st.columns(3)
     
-    # Rainfall Zone Selection
-    rainfall_zones = df['Zon Taburan Hujan'].unique()
-    rainfall_zone = st.sidebar.selectbox('Rainfall Zone', rainfall_zones)
+    with col1:
+        # Rainfall Zone Selection
+        rainfall_zones = df['Zon Taburan Hujan'].unique()
+        rainfall_zone = st.selectbox('Rainfall Zone', rainfall_zones)
     
-    # Soil Class Selection
-    soil_classes = df['Kelas Tanah'].unique()
-    soil_class = st.sidebar.selectbox('Soil Class', soil_classes)
+    with col2:
+        # Soil Class Selection
+        soil_classes = df['Kelas Tanah'].unique()
+        soil_class = st.selectbox('Soil Class', soil_classes)
     
-    # Topography Selection
-    topographies = df['Topografi'].unique()
-    topography = st.sidebar.selectbox('Topography', topographies)
+    with col3:
+        # Topography Selection
+        topographies = df['Topografi'].unique()
+        topography = st.selectbox('Topography', topographies)
     
     # Year Selection
     max_year = df['Tahun Tuai'].max()
     min_year = df['Tahun Tuai'].min()
-    year = st.sidebar.slider('Planting Year', min_value=min_year, max_value=max_year, value=min_year)
+    year = st.slider('Planting Year', min_value=min_year, max_value=max_year, value=min_year)
     
-    # Predict SYP
-    if st.sidebar.button('Calculate SYP'):
+    # Calculate Button
+    if st.button('Calculate Site Yield Potential'):
         syp = predict_syp(df, rainfall_zone, soil_class, topography, year)
         
         if syp is not None:
@@ -128,10 +157,13 @@ def main():
         else:
             st.error('No matching data found. Please adjust your parameters.')
     
-    # Dataset Information
-    st.sidebar.markdown('### Dibangunkan oleh Rafizan Samian - Jabatan Strategi & Transformasi FELDA')
-
-
+    # Soil Class Table
+    st.subheader('Soil Classification Details')
+    soil_class_df = get_soil_class_table()
+    st.dataframe(soil_class_df, use_container_width=True)
+    
+    # Footer
+    st.markdown('### Developed by Rafizan Samian - FELDA Strategy & Transformation Department')
 
 if __name__ == '__main__':
     main()
